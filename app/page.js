1f01a2e9bbf5869e424e25d8224c433efabfd41b
ceo-unknown <unknownsociety20@gmail.com>
2024-08-30 18:40:09 +0530

    new components

--- app/page.js
+++ app/page.js
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <div className="row">
+      <div className="row">
         <div className="col-12 col-md-8 col-lg-8">
           <Post />
         </div>
         <div className="col-12 col-md-4 col-lg-4">
           <SuggestPost />
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
